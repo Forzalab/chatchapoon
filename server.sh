08c70910983fc5c977491eae4b456ee7dc56f5f8
Forzalab <1041841@my.scccd.edu>
2026-05-03 04:36:31 -0700

    feat: prototype hsk

--- server.sh
+++ server.sh
@@ -1,12 +1,1 @@
-
-#!/bin/bash
-find src -name "*.java" > sources.txt
-if javac -cp .:lib/* @sources.txt -d out/ 2>&1; then
-	echo "build complete!"
-	echo "starting server..."
-	pid=$(lsof -ti :4267) && { echo "Process found on port 4267 (PID: $pid). Killing it..."; kill $pid; }
-	java -cp out/:lib/* server.GameServer
-else
-	echo "build failed."
-	exit 1
-fi
+/home/ho_1041841/chat-cha-poom/sh/server.sh
--- src/server/GameServer.java
+++ src/server/GameServer.java
@@ -7,17 +7,16 @@ import shared.Protocol;
 public class GameServer {
     public static void main(String[] args) throws Exception {
         // socket handshake
-	try {
-		ServerSocket ss = new ServerSocket(Protocol.PORT);
-	        System.out.println("Starting on port " + Protocol.PORT);
-		while (true) {
-			Socket s = ss.accept();
-		     	System.out.println("Client connected!");
-//			ss.close();
-		}
-	}
-	catch (Exception e) {
-		System.out.println("Exception caught: " + e);
-	}
+    	try {
+    		ServerSocket ss = new ServerSocket(Protocol.PORT);
+    	    System.out.println("Starting on port " + Protocol.PORT);
+    		while (true) {
+    			Socket s = ss.accept();
+    			new Thread(new ClientHandler(s)).start();
+    		    System.out.println("Client connected!");
+    		}
+        } catch (Exception e) {
+        		System.out.println("Exception caught: " + e);
+        }
     }
 }
