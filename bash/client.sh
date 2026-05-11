51e019f3e8fd107837831d4efefbcddc0b4eb203
Forzalab <1041841@my.scccd.edu>
2026-05-11 02:26:49 -0700

    fix: bash files

--- bash/client.sh
+++ bash/client.sh
@@ -12,11 +12,12 @@ done
 # build and run
 find src -name "*.java" > sources.txt
 if javac -cp .:lib/* @sources.txt -d out/ 2>&1; then
-	echo "build complete!"
-	echo "starting client..."
-	sleep 0.3
-    java -cp out/:lib/* -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 client.GameClient
+    echo "build complete!"
+    echo "starting client..."
+    sleep 0.3
+    
+    java -cp out/:lib/* -Dfile.encoding=UTF-8 -Dstdout.encoding=UTF-8 client.GameClient "$USER"
 else
-	echo "build failed."
-	exit 1
+    echo "build failed."
+    exit 1
 fi
--- bash/server.sh
+++ bash/server.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+PORT=$(grep "public static final int PORT =" ./src/shared/Protocol.java | sed 's/[^0-9]*//g')
+
+if [ -z "$PORT" ]; then
+    echo "no port def"
+    exit 1
+fi
+
+pid=$(lsof -ti :$PORT) && { echo "Process found on port $PORT (PID: $pid).";}
+if [ -n "$PID" ] ; then
+    if kill "$PID" 2>/dev/null; then
+        echo "Process $PID terminated!"
+        sleep 0.5
+    else
+        echo "Port $PORT is already in use!"
+        echo "Close any server instance, or go to nezt port pls."
+        exit 1
+    fi
+fi
+
 # symlink stage
 #!/usr/bin/env bash
 PARENT_DIR="$(dirname "$(dirname "$(realpath "$0")")")"
@@ -13,7 +32,6 @@ find src -name "*.java" > sources.txt
 if javac -cp .:lib/* @sources.txt -d out/ 2>&1; then
 	echo "build complete!"
 	echo "starting server..."
-	pid=$(lsof -ti :4267) && { echo "Process found on port 4267 (PID: $pid). Killing it..."; kill $pid; }
 	sleep 0.3
 	java -cp out/:lib/* server.GameServer
 else
